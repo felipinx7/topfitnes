9930412c2511969f291afc2b4e91ab6ab488e0c1
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-07-21 15:21:14 -0300

    .

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,15 @@
-
+import Fundo from '../../assets/image/Textura(quadrados).png'
 
 export function Login(){
+
+    
     return(
         <main className="w-full h-full">
-            <div className="w-1/2 h-full  ">
-            
+            <div className="w-1/2 h-full bg-neutras-400">
+            <img className='w-full h-full' src='../../assets/image/Textura(quadrados).png' alt="" />
+                <div>
+
+                </div>
             
             </div>
 
