6d62bec86e0b128ae26fbd751036ef8d5603eb8a
Felipe Lima <flimalimafelipek@gmail.com>
2025-07-21 16:42:27 -0300

    feat: inicial cards ladind page

---
+++ src/dto/data-cards-lading-page.ts
@@ -0,0 +1,8 @@
+import { Icon } from "next/dist/lib/metadata/types/metadata-types";
+
+export interface DataCardsLadingPage {
+  titulo: string;
+  subtitulo?: string;
+  descricao: string;
+  icone: Icon;
+}
---
+++ src/pages/lading-page/components/card-beneficio.tsx
@@ -0,0 +1,7 @@
+export default function CardBeneficios(){
+    return(
+        <article className="flex flex-col">
+
+        </article>
+    )
+}
