10d97e913f05afc9beb45cf03d8c7d6a8249c4c2
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-07-21 16:41:44 -0300

    feat: Login start

---
+++ src/app/login/page.tsx
@@ -0,0 +1,4 @@
+import { Login } from "@/pages/login/login";
+export default function LoginPage(){
+  return Login()
+}
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,18 +1,26 @@
-import Fundo from '../../assets/image/Textura(quadrados).png'
+import Fundo from "../../assets/image/Textura-quadrados.png";
 
-export function Login(){
+export function Login() {
+  return (
+    <main className="w-screen h-screen flex flex-row">
 
-    
-    return(
-        <main className="w-full h-full">
-            <div className="w-1/2 h-full bg-neutras-400">
-            <img className='w-full h-full' src='../../assets/image/Textura(quadrados).png' alt="" />
-                <div>
+      <div className="w-1/3 h-full bg-neutras-400 relative">
+      {/* Tetura do fundo */}
+        <div
+          style={{
+            backgroundImage: `url(${Fundo.src})`,
+            backgroundSize: "cover",
+          }}
+          className="absolute inset-0 bg-cover bg-center z-0"
+        >
+          <div className="bg-gradient-to-tl  from-verde-100/20 absolute bottom-0 right-0 to-transparent to-50% w-2/3 h-2/3"></div>
+        </div>
+        
+        {" "}
+          <form action="" method="POST" className="w-full h-full flex-col flex items-center justify-center">
 
-                </div>
-            
-            </div>
-
-        </main>
-    )
+          </form>
+      </div>
+    </main>
+  );
 }
