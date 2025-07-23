d12c53f22a6bb0f98b740ea3474de1269202cd4e
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-07-23 16:16:03 -0300

    feat: Start of the aluno's homepage

--- src/pages/home-aluno/homeAluno.tsx
+++ src/pages/home-aluno/homeAluno.tsx
@@ -1,15 +1,35 @@
 'use client'
 
 import { useState } from "react"
+
 import { SideBar } from "./sections/sideBar"
+
+import { Inicio } from "./sections/Inicio";
 
 export function HomeAluno(){
     const [section,setSection] = useState(1);
     
+    function RenderSection(section: number){
+  switch (section) {
+    case 1:
+      return <Inicio/>;
+    case 2:
+      return <div>Seção 2</div>;
+    case 3:
+      return <div>Seção 3</div>;
+    case 4:
+      return <div>Seção 4</div>;
+    case 5:
+      return <div>Seção 5</div>;
+  }
+
+
+    }
 
     return(
         <main className="w-screen h-screen bg-neutras-300 flex">
-           <div className="w-1/5 h-full"> <SideBar setId={setSection} id={section} ></SideBar></div>       
+           <div className="w-1/5 h-full"> <SideBar setId={setSection} id={section} ></SideBar></div>
+           <div className="w-4/5 h-full"> {RenderSection(section)}</div>       
         </main>
     )
 }
