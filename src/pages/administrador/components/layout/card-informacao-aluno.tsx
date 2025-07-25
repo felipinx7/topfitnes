3d7fda928b1a0cf485190f71bb27946f583edead
Felipe Lima <flimalimafelipek@gmail.com>
2025-07-25 16:28:13 -0300

    feat: update in section admin

--- src/pages/administrador/components/layout/card-informacao-aluno.tsx
+++ src/pages/administrador/components/layout/card-informacao-aluno.tsx
@@ -5,6 +5,7 @@ import { DataAluno } from "@/dto/data-aluno";
 import Image from "next/image";
 import { useState } from "react";
 import ModalFormularioCardCliente from "./modal-formulario-card-cliente";
+import DeleteClienteAdministrador from "@/services/routes/administrador/delete/delete-cliente-administrador";
 
 export default function CardInformacaoAluno(props: DataAluno) {
   // Estado utilizados no componente
@@ -16,9 +17,17 @@ export default function CardInformacaoAluno(props: DataAluno) {
   function handleVisibilityModal() {
     setOpenModal((prev) => !prev);
   }
+
+  async function deletarUsuário(id: string) {
+    const response = DeleteClienteAdministrador(id);
+    console.log("Excluido", response);
+    return response;
+  }
   return (
     // container card
-    <article className={`w-full ease-in-out h-auto rounded-2xl bg-[#d8ffe2] transition-all duration-500 p-4 flex flex-col gap-0 ${openModal ? "gap-4" : "gap-0"}`}>
+    <article
+      className={`w-full ease-in-out h-auto rounded-2xl bg-[#d8ffe2] transition-all duration-500 p-4 flex flex-col gap-0 ${openModal ? "gap-4" : "gap-0"}`}
+    >
       {/* Linha principal: foto + nome + botões */}
       <div className="w-full ease-in-out duration-500 transition-all flex items-center justify-between">
         <div className="flex items-center gap-3">
@@ -45,7 +54,7 @@ export default function CardInformacaoAluno(props: DataAluno) {
           >
             <IconeLapis className="text-black group-hover:text-white" />
           </button>
-          <button className="group cursor-pointer hover:bg-verde-100 transition-all duration-300 rounded-[5.97px] p-2 flex items-center justify-center">
+          <button onClick={() => deletarUsuário(props.id)} className="group cursor-pointer hover:bg-verde-100 transition-all duration-300 rounded-[5.97px] p-2 flex items-center justify-center">
             <IconeLiixeira className="text-black group-hover:text-white" />
           </button>
         </div>
