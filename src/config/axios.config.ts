7d1bc2a884840049f5c3d4c8966a9adcadd478a6
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-13 14:09:46 -0300

    fix: backend url

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const BASEURL = "http://localhost:4000"
+export const BASEURL = "https://api.topfitnes.com.br/api"
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://api.topfitnes.com.br/api",
   withCredentials: true,
 });
